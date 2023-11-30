d90137a3c48c159de06bb2dcd6573c39812d433a
daniyeg <daniyeg2@gmail.com>
2023-11-30 14:15:59 +0330

    added pre-calculated nice value to weight value conversion

--- proc.c
+++ proc.c
@@ -89,6 +89,20 @@ rbdelete(struct redBlackTree *tree, struct proc *node)
 
 // --------------------------------------------
 
+// Nice value to weight value conversion.
+// Roughly equivalent to 1024 / (1.25 ^ nice_value)
+// Nice value here is in the same range as nice value in linux kernel, between -20 and 19.
+static const int prio_to_weight[40] = {
+ /* -20 */     88761,     71755,     56483,     46273,     36291,
+ /* -15 */     29154,     23254,     18705,     14949,     11916,
+ /* -10 */      9548,      7620,      6100,      4904,      3906,
+ /*  -5 */      3121,      2501,      1991,      1586,      1277,
+ /*   0 */      1024,       820,       655,       526,       423,
+ /*   5 */       335,       272,       215,       172,       137,
+ /*  10 */       110,        87,        70,        56,        45,
+ /*  15 */        36,        29,        23,        18,        15,
+};
+
 // Initialize the red black tree
 void
 rbinit(struct redBlackTree *tree, char *lockName)
@@ -144,8 +158,8 @@ insertproc(struct redBlackTree *tree, struct proc *p)
 
   if (tree->count < NPROC) // If the tree isn't full
   {
-    // Calculate the weight of the process based on its nice value.
-    p->weightValue = 0; // TODO: calculate weight
+    // Determine the weight of the process based on its nice value.
+    p->weightValue = prio_to_weight[p->niceValue + 20];
 
     // Insert process into the tree.
     // All properties that change about the tree is done within rbinsert.
