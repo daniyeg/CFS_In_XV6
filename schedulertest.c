21c98b42a90585ed673a7f53aa518b261edf5daa
daniyeg <daniyeg2@gmail.com>
2023-12-07 13:44:16 +0330

    added nice test to schedulertest.c

---
+++ schedulertest.c
@@ -0,0 +1,55 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+void
+cpuproc(void)
+{
+  for(;;) {}
+}
+
+void
+nicetest()
+{
+  int pid1 = fork();
+  if (pid1 == 0)
+  {
+    nice(-10);
+    cpuproc();
+    exit();
+  }
+  int pid2 = fork();
+  if (pid2 == 0)
+  {
+    nice(10);
+    cpuproc();
+    exit();
+  }
+
+  printf(1, "pid1:%d pid2:%d\n", pid1, pid2);
+
+  // Busy wait
+  int temp;
+  for (int i = 0; i < 1000000000; i++)
+  {
+    temp *= 2;
+    temp /= 2;
+  }
+
+  // Get process stats after waiting
+  ps();
+
+  // Kill forks
+  kill(pid1);
+  kill(pid2);
+
+  wait();
+  wait();
+}
+
+int
+main(void)
+{
+  nicetest();
+  exit();
+}
